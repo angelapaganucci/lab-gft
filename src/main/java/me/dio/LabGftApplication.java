b25052e68bd35602c3d342f84abe589436e58975
Angela Paganucci <angela.luiz@db1.com.br>
2024-06-17 17:46:43 -0300

    Initial commit

---
+++ src/main/java/me/dio/LabGftApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabGftApplication.class, args);
+    }
+
+}
